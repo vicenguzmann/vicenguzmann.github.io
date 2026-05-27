7fc49f8ae9b1c5de0d35e7fb49c7a1fcb9b82b07
Vicen <vicenguzmann@gmail.com>
2026-05-27 18:33:09 +0200

    Updates

--- assets/NotFound-D8JcI-Ji.js
+++ assets/NotFound-D8JcI-Ji.js
@@ -1,4 +1,4 @@
-import{j as e}from"./ui-iJIdZ4Fb.js";import{r as t}from"./react-BSxZn5Zl.js";import{c as r,u as a}from"./index-Cqnc83Ne.js";import{u as s}from"./router-C5ClFUnI.js";import"./shadcn-BjlOjBMC.js";import"./tanstack-DBgKTtll.js";
+import{j as e}from"./ui-iJIdZ4Fb.js";import{r as t}from"./react-BSxZn5Zl.js";import{c as r,u as a}from"./index-C9anX7XJ.js";import{u as s}from"./router-C5ClFUnI.js";import"./shadcn-BjlOjBMC.js";import"./tanstack-DBgKTtll.js";
 /**
  * @license lucide-react v0.462.0 - ISC
  *
--- assets/twitter-CHfUxC3w.js
+++ assets/twitter-CHfUxC3w.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-Cqnc83Ne.js";
+import{c as t}from"./index-C9anX7XJ.js";
 /**
  * @license lucide-react v0.462.0 - ISC
  *
